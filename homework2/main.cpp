8e953df1fae99939ffd65ce77efe5bc3ba9747e2
Egor <90556524+KosolapovEgor@users.noreply.github.com>
2021-11-30 01:19:28 +0300

    Update main.cpp

--- homework2/main.cpp
+++ homework2/main.cpp
@@ -4,7 +4,6 @@
 
 
 int main(int argc, char** argv) {
-    if (argc == 2) {
     double y_0 = 0, v_x = 0, v_y = 0, g = 9.81, answer = 0;
     double check, tmp, t_collision;
     int direction = 1;
@@ -74,5 +73,4 @@ int main(int argc, char** argv) {
     delete[] X;
     delete[] Y;
     std::cout << answer << std::endl;
-}
 }
